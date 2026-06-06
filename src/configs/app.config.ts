98b660974972930588df79a7bf2074bc07a755e1
Bhone-Myat-Kyaw <bmkyawstudent14@gmail.com>
2026-06-06 10:57:27 +0630

    adding proxy for http request to work
    adding vercel.json to add proxy so that http (not https) requests work

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -9,7 +9,7 @@ export type AppConfig = {
 }
 
 const appConfig: AppConfig = {
-    apiPrefix: 'http://152.42.244.67/api', //'http://localhost:3000/api',
+    apiPrefix: '/api/proxy', //'http://152.42.244.67/api', //'http://localhost:3000/api',
     authenticatedEntryPath: '/home',
     unAuthenticatedEntryPath: '/sign-in',
     locale: 'en',
